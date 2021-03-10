ed99661f6fa7b045d2ade2b5ddfe568f20b7a00f
lionelritchie29 <lionelritchie29@github.com>
2021-03-10 16:12:19 +0700

    added overlay for safety sheet;

--- src/components/safety/SafetySheet.js
+++ src/components/safety/SafetySheet.js
@@ -18,13 +18,20 @@ const SafetySheet = () => {
   return (
     <section className="safety-container min-h-screen min-w-screen">
       <div
-        className="min-h-screen min-w-screen py-24 px-6 lg:p-72 flex items-end"
+        className="min-h-screen relative min-w-screen py-24 px-6 lg:p-72 flex items-end"
         style={{
           backgroundImage: `url(${
             windowWidth <= 640 ? safetyBgMobile : safetyBgDesktop
           })`,
         }}
       >
+        <div
+          className="absolute top-0 left-0 w-full h-full"
+          style={{
+            background:
+              "linear-gradient(90deg, transparent, rgba(0, 0, 0, 0.4))",
+          }}
+        ></div>
         <div>
           <button
             className="bg-primary font-semibold text-white rounded-full text-lg px-4 lg:px-8 lg:py-2"
