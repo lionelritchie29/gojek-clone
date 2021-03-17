4a1f0b0c111b1adfa09616a2b16e59b616273973
lionelritchie29 <lionelritchie29@github.com>
2021-03-17 15:12:23 +0700

    fixed responsive for product slider

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,5 +1,3 @@
-import { useRef } from "react";
-
 const ProductItem = ({ product }) => {
   return (
     <div>
@@ -8,7 +6,7 @@ const ProductItem = ({ product }) => {
         style={{ backgroundColor: product.innerBgColor }}
       >
         <div
-          className="h-80 rounded-3xl relative"
+          className="h-72 md:h-80 rounded-3xl relative"
           style={{ backgroundColor: product.bgColor }}
         >
           <div className="absolute bottom-0 left-0 w-full">
@@ -18,16 +16,16 @@ const ProductItem = ({ product }) => {
               alt="background"
             />
           </div>
-          <div className="flex h-full flex-col p-10">
+          <div className="flex h-full flex-col p-5 md:p-10">
             {product.subProducts.map((sub) => (
-              <img key={sub} className="mb-8 mr-5 w-36" src={sub} />
+              <img key={sub} className="mb-8 mr-5 w-36" alt="" src={sub} />
             ))}
           </div>
         </div>
 
-        <div className="text-white p-8 hidden" id="product-detail">
+        <div className="text-white p-6 hidden" id="product-detail">
           <h3 className="text-xl font-bold">{product.category}</h3>
-          <p className="text-sm mt-4">{product.description}</p>
+          <p className="text-sm mt-2">{product.description}</p>
         </div>
       </div>
     </div>
--- src/components/products/ProductSlider.js
+++ src/components/products/ProductSlider.js
@@ -1,9 +1,11 @@
 import { useLayoutEffect, useState, useRef } from "react";
 import TinySlider from "tiny-slider-react";
 import ProductItem from "./ProductItem";
+import useWindowWidth from "../../hooks/useWindowWidth";
 
 const ProductSlider = () => {
   const slider = useRef();
+  const windowWidth = useWindowWidth();
 
   const [products] = useState([
     {
@@ -87,12 +89,27 @@ const ProductSlider = () => {
     const { ref } = slider.current;
     const { index, slideItems } = slider.current.slider.getInfo();
 
-    ref.classList.add("-m-9");
-    slideItems[index].style.marginRight = "4.5rem";
-    slideItems[index].firstChild.classList.add("product-active");
-    slideItems[index]
-      .querySelector("#product-detail")
-      .classList.remove("hidden");
+    if (windowWidth > 640) ref.classList.add("-m-9");
+
+    if (windowWidth >= 1024) {
+      slideItems[index].style.marginRight = "4.5rem";
+      slideItems[index].firstChild.classList.add("product-active");
+      slideItems[index].firstChild.firstChild.classList.add("-mt-16");
+    } else if (windowWidth >= 768) {
+      slideItems[index].style.marginRight = "5.5rem";
+      slideItems[index].firstChild.classList.add("product-active");
+      slideItems[index].firstChild.firstChild.classList.add("-mt-16");
+    } else {
+      slideItems[index].style.marginRight = "1rem";
+      slideItems[index].firstChild.style.transform = "scaleY(1.1)";
+      slideItems[index].firstChild.classList.add("w-52");
+    }
+
+    if (windowWidth > 640) {
+      slideItems[index]
+        .querySelector("#product-detail")
+        .classList.remove("hidden");
+    }
   });
 
   const onIndexChanged = (info, eventName) => {
@@ -103,41 +120,72 @@ const ProductSlider = () => {
     const productDetail = slideItems[index].querySelector("#product-detail");
 
     // reset previous slide
-    previousProductDetail.classList.add("hidden");
-    slideItems[indexCached].style.marginRight = "0";
-    slideItems[indexCached].firstChild.classList.remove("product-active");
+    if (windowWidth > 640) {
+      previousProductDetail.classList.add("hidden");
+      slideItems[indexCached].firstChild.firstChild.classList.remove("-mt-16");
+    }
+
+    if (windowWidth >= 768) {
+      slideItems[indexCached].firstChild.classList.remove("product-active");
+      slideItems[indexCached].style.marginRight = "0";
+    } else {
+      slideItems[indexCached].style.marginRight = "0";
+      slideItems[indexCached].firstChild.style.transform = "scaleY(1)";
+      slideItems[indexCached].firstChild.classList.remove("w-52");
+    }
 
     // set current slide as active
-    productDetail.classList.remove("hidden");
-    slideItems[index].style.marginRight = "4.5rem";
-    slideItems[index].firstChild.classList.add("product-active");
+    if (windowWidth > 640) {
+      productDetail.classList.remove("hidden");
+      slideItems[index].firstChild.firstChild.classList.add("-mt-16");
+    }
+
+    if (windowWidth >= 1024) {
+      slideItems[index].firstChild.classList.add("product-active");
+      slideItems[index].style.marginRight = "4.5rem";
+    } else if (windowWidth >= 768) {
+      slideItems[index].firstChild.classList.add("product-active");
+      slideItems[index].style.marginRight = "5.5rem";
+    } else {
+      slideItems[index].style.marginRight = "1rem";
+      slideItems[index].firstChild.style.transform = "scaleY(1.1)";
+      slideItems[index].firstChild.classList.add("w-52");
+    }
   };
 
   const settings = {
     mouseDrag: false,
     navContainer: "#product-nav",
-    // prevButton: "#perk-prev",
-    // nextButton: "#perk-next",
+    controls: false,
     navAsThumbnails: true,
     center: true,
-    gutter: 40,
+
     responsive: {
-      640: {
+      300: {
         items: 3,
+        mouseDrag: true,
+        gutter: 20,
+        fixedWidth: 192,
       },
       768: {
         items: 3,
+        mouseDrag: false,
+        gutter: 40,
+        fixedWidth: false,
       },
       1024: {
         items: 5,
+        mouseDrag: false,
+        gutter: 40,
+        fixedWidth: false,
       },
     },
   };
 
   return (
-    <div className="lg:px-36 py-36 mx-auto">
+    <div className="lg:px-36 py-8 mx-auto">
       <TinySlider
-        className="py-24"
+        className="py-28"
         settings={settings}
         onIndexChanged={onIndexChanged}
         ref={slider}
@@ -148,6 +196,7 @@ const ProductSlider = () => {
       </TinySlider>
 
       <ul
+        style={{ display: windowWidth <= 640 ? "none" : "flex" }}
         id="product-nav"
         className="text-white flex w-full lg:px-36 mt-12 lg:w-3/5 mx-auto justify-between"
       >
