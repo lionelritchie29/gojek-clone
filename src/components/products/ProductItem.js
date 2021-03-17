2c4d71cbb5c1991ebdd0730fede82917a7e1b14b
lionelritchie29 <lionelritchie29@github.com>
2021-03-17 13:59:39 +0700

    added style for product in product slider (not responsive)

--- src/components/products/ProductItem.js
+++ src/components/products/ProductItem.js
@@ -1,6 +1,36 @@
+import { useRef } from "react";
+
 const ProductItem = ({ product }) => {
   return (
-    <div style={{ backgroundColor: product.bgColor }}>{product.category}</div>
+    <div>
+      <div
+        className="rounded-3xl"
+        style={{ backgroundColor: product.innerBgColor }}
+      >
+        <div
+          className="h-80 rounded-3xl relative"
+          style={{ backgroundColor: product.bgColor }}
+        >
+          <div className="absolute bottom-0 left-0 w-full">
+            <img
+              className="w-full object-contain"
+              src={product.background}
+              alt="background"
+            />
+          </div>
+          <div className="flex h-full flex-col p-10">
+            {product.subProducts.map((sub) => (
+              <img key={sub} className="mb-8 mr-5 w-36" src={sub} />
+            ))}
+          </div>
+        </div>
+
+        <div className="text-white p-8 hidden" id="product-detail">
+          <h3 className="text-xl font-bold">{product.category}</h3>
+          <p className="text-sm mt-4">{product.description}</p>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/components/products/ProductSlider.js
+++ src/components/products/ProductSlider.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useLayoutEffect, useState, useRef } from "react";
 import TinySlider from "tiny-slider-react";
 import ProductItem from "./ProductItem";
 
 const ProductSlider = () => {
+  const slider = useRef();
+
   const [products] = useState([
     {
       id: 1,
@@ -19,6 +21,7 @@ const ProductSlider = () => {
       background:
         "https://lelogama.go-jek.com/prime/upload/image/product-intro-card-bg-transport.svg",
       bgColor: "#00AA13",
+      innerBgColor: "#257226",
     },
     {
       id: 2,
@@ -34,6 +37,7 @@ const ProductSlider = () => {
       background:
         "https://lelogama.go-jek.com/prime/upload/image/product-intro-card-bg-payments.svg",
       bgColor: "#00AED6",
+      innerBgColor: "#0085AD",
     },
     {
       id: 3,
@@ -48,6 +52,7 @@ const ProductSlider = () => {
       background:
         "https://lelogama.go-jek.com/prime/upload/image/product-intro-card-bg-food.svg",
       bgColor: "#EE2737",
+      innerBgColor: "#A6192E",
     },
     {
       id: 4,
@@ -60,6 +65,7 @@ const ProductSlider = () => {
       background:
         "https://lelogama.go-jek.com/prime/upload/image/product-intro-card-bg-business.svg",
       bgColor: "#93328E",
+      innerBgColor: "#720062",
     },
     {
       id: 5,
@@ -73,16 +79,48 @@ const ProductSlider = () => {
       background:
         "https://lelogama.go-jek.com/prime/upload/image/product-intro-card-bg-entertainment.svg",
       bgColor: "#DF1995",
+      innerBgColor: "#890C58",
     },
   ]);
 
+  useLayoutEffect(() => {
+    const { ref } = slider.current;
+    const { index, slideItems } = slider.current.slider.getInfo();
+
+    ref.classList.add("-m-9");
+    slideItems[index].style.marginRight = "4.5rem";
+    slideItems[index].firstChild.classList.add("product-active");
+    slideItems[index]
+      .querySelector("#product-detail")
+      .classList.remove("hidden");
+  });
+
+  const onIndexChanged = (info, eventName) => {
+    const { index, indexCached, slideItems } = info;
+    const previousProductDetail = slideItems[indexCached].querySelector(
+      "#product-detail"
+    );
+    const productDetail = slideItems[index].querySelector("#product-detail");
+
+    // reset previous slide
+    previousProductDetail.classList.add("hidden");
+    slideItems[indexCached].style.marginRight = "0";
+    slideItems[indexCached].firstChild.classList.remove("product-active");
+
+    // set current slide as active
+    productDetail.classList.remove("hidden");
+    slideItems[index].style.marginRight = "4.5rem";
+    slideItems[index].firstChild.classList.add("product-active");
+  };
+
   const settings = {
-    mouseDrag: true,
-    // navContainer: "#perk-dots",
+    mouseDrag: false,
+    navContainer: "#product-nav",
     // prevButton: "#perk-prev",
     // nextButton: "#perk-next",
+    navAsThumbnails: true,
     center: true,
-    gutter: 20,
+    gutter: 40,
     responsive: {
       640: {
         items: 3,
@@ -97,12 +135,26 @@ const ProductSlider = () => {
   };
 
   return (
-    <div>
-      <TinySlider settings={settings}>
+    <div className="lg:px-36 py-36 mx-auto">
+      <TinySlider
+        className="py-24"
+        settings={settings}
+        onIndexChanged={onIndexChanged}
+        ref={slider}
+      >
         {products.map((product) => (
           <ProductItem product={product} key={product.id} />
         ))}
       </TinySlider>
+
+      <ul
+        id="product-nav"
+        className="text-white flex w-full lg:px-36 mt-12 lg:w-3/5 mx-auto justify-between"
+      >
+        {products.map((product) => (
+          <li key={product.id}>{product.category}</li>
+        ))}
+      </ul>
     </div>
   );
 };
